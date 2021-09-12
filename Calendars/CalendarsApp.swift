7aeba99f103fe99f2b2800f7d859a04a6c43b405
oogioogi <detacl73@gmail.com>
2021-09-12 18:37:52 +0900

    Initial Commit

---
+++ Calendars/CalendarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarsApp.swift
+//  Calendars
+//
+//  Created by 이용석 on 2021/09/12.
+//
+
+import SwiftUI
+
+@main
+struct CalendarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
